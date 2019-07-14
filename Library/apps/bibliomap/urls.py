b4139a739a038f06cde9b3c159ea33da3512fd09
lance <hugofer93@gmail.com>
2019-07-13 20:27:01 -0500

    config of bibliomap app in project

---
+++ Library/apps/bibliomap/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'bibliomap'
+
+urlpatterns = [
+    #path('', views.Home.as_view(), name='home'),
+]
--- Library/urls.py
+++ Library/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .settings import DEBUG as debug
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Library.apps.bibliomap.urls', namespace='bibliomap')),
 ]
+
+if debug:
+    from django.conf.urls.static import static
+
+    from .settings import (
+        MEDIA_URL as mediaUrl,
+        MEDIA_ROOT as mediaRoot,
+    )
+
+    urlpatterns += static(mediaUrl, document_root=mediaRoot)
